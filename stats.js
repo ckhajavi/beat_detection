274be84708aa8dc13dda8f05925389640e7c372c
ckhajavi <cara.khajavi@gmail.com>
2016-06-01 10:14:12 -0700

    adding stat's js to make code cleaner

---
+++ stats.js
@@ -0,0 +1,36 @@
+function squareAndSum(arr){
+        var sum = 0;
+        var element;
+        for (i = 0; i<arr.length; ++i){
+            element = Math.pow(arr[i],2);
+            element = Math.sqrt(element);
+            sum = sum + element;
+
+    	}
+        return sum;
+    }
+
+Array.max = function( array ){
+    return Math.max.apply( Math, array );
+};
+
+
+    
+function average(arr){
+    var sum = 0;
+    for( i = 0; i < arr.length; ++i){
+        sum = sum + arr[i];
+    }
+    return sum/44;
+}
+
+function variance(average, arr){
+    var variance = 0;
+    var diff;
+    for(i =0; i < arr.length; ++i){
+        diff = arr[i] - average;
+        variance = variance + Math.pow(diff, 2);
+
+    }
+    return variance/1024;
+}
